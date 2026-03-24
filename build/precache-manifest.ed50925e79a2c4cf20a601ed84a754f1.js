87269c29be6e1c65208fd5ce519e5f9d8eda7401
victormartinss0811 <victormartins249@gmail.com>
2026-03-24 16:02:57 +0000

    Adicionando pipeline e frontend

---
+++ build/precache-manifest.ed50925e79a2c4cf20a601ed84a754f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baaa8167ff168c1dd55ebfd63d2dd798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7209697a56416a433f2",
+    "url": "/static/css/main.4bf56663.chunk.css"
+  },
+  {
+    "revision": "4a4485690fe0b2554e2b",
+    "url": "/static/js/2.59d474c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.59d474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7209697a56416a433f2",
+    "url": "/static/js/main.8e7d72d0.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "d83e9da03be4df7942a9a2708260fda4",
+    "url": "/static/media/MBA.d83e9da0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
